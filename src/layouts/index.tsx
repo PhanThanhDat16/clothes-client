bab42a43259da0a7f3bb91816ed9038885cbb11c
PhanThanhDat16 <phandat1662004@gmail.com>
2025-03-28 09:59:21 +0700

    setup release

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,20 @@
-const index = () => {
-  return <div></div>
+// Libs
+import { Outlet } from 'react-router-dom'
+
+// Components
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
+const MainLayout = () => {
+  return (
+    <div className="main-layout">
+      <Header />
+      <div className="main-layout__content">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
-export default index
+export default MainLayout
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,5 +1,26 @@
-const index = () => {
-  return <div></div>
+// Libs
+import { Route, Routes } from 'react-router-dom'
+
+// Page
+import HomePage from '@/pages/HomePage'
+
+// Router
+import { HOME_PAGE } from '@/constants'
+
+import MainLayout from '@/layouts'
+
+const AppRouters = () => {
+  return (
+    <Routes>
+      <Route path={HOME_PAGE} element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+
+        {/* Ví dụ trang danh mục
+          <Route path={CATEGORIES_PAGE} element={<Category />} />
+        */}
+      </Route>
+    </Routes>
+  )
 }
 
-export default index
+export default AppRouters
